4bbb7aaf513137cfb0725bc53186332b4c250214
Max <68688024+Maggos-sketch@users.noreply.github.com>
2021-03-13 20:40:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let arr = [];
+  let sNum = n.toString();
+  for (letter of sNum) {
+    arr.unshift(letter);
+  }
+  let r = arr.join("");
+  let nn = parseInt(r, 10);
+  return nn;
 }
